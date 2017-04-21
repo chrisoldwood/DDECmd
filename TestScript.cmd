79aee46c6792524f39cd3c85e9b45b40711478a8
Chris Oldwood <gort@cix.co.uk>
2017-04-21 17:26:16 +0100

    Enhanced TestScript so that you can specify the build type.

--- TestScript.cmd
+++ TestScript.cmd
@@ -2,33 +2,52 @@
 rem **********************************************************************
 rem Smoke test script to invoke DDECmd with various command lines.
 rem **********************************************************************
+setlocal
 
+:handle_help_request
+if /i "%~1" == "-?"     call :usage & exit /b 0
+if /i "%~1" == "--help" call :usage & exit /b 0
+
+:check_args
+if /i "%~1" == "" call :usage & exit /b 1
+
+:set_build
+if /i "%~1" == "debug"   set build=Debug
+if /i "%~1" == "release" set build=Release
+if /i "%build%"== "" (
+	echo ERROR: Invalid build type "%~1"
+	call :usage
+	exit /b 0
+)
+
+set root=%~dp0
+set folder=%root%%build%
 set exitCode=0
 
 echo ----------------------------------------------------------------------
 
-Debug\DDECmd --version
-if errorlevel 1 call :test_failed & set exitCode=1
+%folder%\DDECmd --version
+if %errorlevel% neq 0 call :test_failed & set exitCode=1
 
 echo ----------------------------------------------------------------------
 
-Debug\DDECmd --help
-if errorlevel 1 call :test_failed & set exitCode=1
+%folder%\DDECmd --help
+if %errorlevel% neq 0 call :test_failed & set exitCode=1
 
 echo ----------------------------------------------------------------------
 
-Debug\DDECmd servers
-if errorlevel 1 call :test_failed & set exitCode=1
+%folder%\DDECmd servers
+if %errorlevel% neq 0 call :test_failed & set exitCode=1
 
 echo ----------------------------------------------------------------------
 
-Debug\DDECmd request --server PROGMAN --topic PROGMAN --item Accessories
-if errorlevel 1 call :test_failed & set exitCode=1
+%folder%\DDECmd request --server PROGMAN --topic PROGMAN --item Accessories
+if %errorlevel% neq 0 call :test_failed & set exitCode=1
 
 echo ----------------------------------------------------------------------
 
-Debug\DDECmd request -l "PROGMAN|PROGMAN!Accessories" -of "%%d %%s %%t %%i %%v" -df "ddd dd MMM yy" -tf "hh':'mm tt"
-if errorlevel 1 call :test_failed & set exitCode=1
+%folder%\DDECmd request -l "PROGMAN|PROGMAN!Accessories" -of "%%d %%s %%t %%i %%v" -df "ddd dd MMM yy" -tf "hh':'mm tt"
+if %errorlevel% neq 0 call :test_failed & set exitCode=1
 
 if /i not "%exitCode%" == "0" goto failed
 
@@ -54,4 +73,11 @@ echo **********************************************************************
 echo **********                 TEST FAILED                      **********
 echo **********************************************************************
 echo.
+goto :eof
+
+:usage
+echo.
+echo Usage: %~n0 [debug ^| release]
+echo.
+echo e.g.   %~n0 debug 
 goto :eof
